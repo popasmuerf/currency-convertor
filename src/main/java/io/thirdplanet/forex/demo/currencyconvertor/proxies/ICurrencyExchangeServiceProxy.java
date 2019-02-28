0df7254a458777ce0f82298b35bd0865559d3307
mikeyb <MichaelBrown172@Gmail.com>
2019-02-27 22:20:57 -0500

    added dependency for being a eureka client...

--- src/main/java/io/thirdplanet/forex/demo/currencyconvertor/proxies/ICurrencyExchangeServiceProxy.java
+++ src/main/java/io/thirdplanet/forex/demo/currencyconvertor/proxies/ICurrencyExchangeServiceProxy.java
@@ -1,11 +1,13 @@
 package io.thirdplanet.forex.demo.currencyconvertor.proxies;
-import io.thirdplanet.forex.demo.currencyconvertor.entities.ConcurrencyConversionBean;
 
+import io.thirdplanet.forex.demo.currencyconvertor.entities.ConcurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="forex-service", url="localhost:8000")
+@RibbonClient(name="forex-service")
 public interface ICurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ConcurrencyConversionBean retrieveExchangeValue(
